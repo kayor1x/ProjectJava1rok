4fd19d015b60dc2a287e29bcd80083803dd5e640
Yevhenii <usik.evgen321@gmail.com>
2025-06-15 23:48:26 +0200

    mini fix

--- src/main/java/services/AuthService.java
+++ src/main/java/services/AuthService.java
@@ -41,30 +41,6 @@ public class AuthService {
         return currentUser != null && currentUser.getRole() == models.UserRole.ADMIN;
     }
 
-    public static boolean register(String username, String password) {
-        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
-            Query<User> query = session.createQuery("FROM User WHERE username = :username", User.class);
-            query.setParameter("username", username);
-            User existingUser = query.uniqueResult();
-            if (existingUser != null) {
-                return false;
-            }
-            String hashedPassword = org.mindrot.jbcrypt.BCrypt.hashpw(password, org.mindrot.jbcrypt.BCrypt.gensalt());
-            User newUser = new User();
-            newUser.setUsername(username);
-            newUser.setPassword(hashedPassword);
-            newUser.setRole(models.UserRole.USER);
-            newUser.setCreatedAt(LocalDateTime.now());
-            session.beginTransaction();
-            session.persist(newUser);
-            session.getTransaction().commit();
-            return true;
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return false;
-    }
-
     public static boolean register(String username, String password, String role) {
         try (Session session = HibernateUtil.getSessionFactory().openSession()) {
             Query<User> query = session.createQuery("FROM User WHERE username = :username", User.class);
